ed37db9b523530e0aaaaca0e8a2d51c34d6f82b8
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-21 13:49:06 +0300

    Add files via upload

---
+++ Exams/Christmas/Bag.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        private List<Present> data;
+        
+        public Bag(string color, int capacity)
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+            data = new List<Present>();
+        }
+        public string Color { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Present present)
+        {
+           if(Capacity > Count)
+            {
+                data.Add(present);
+            }          
+        }
+
+        public bool Remove(string name)
+        {
+            Present present = data.FirstOrDefault(x => x.Name == name);
+            return data.Remove(present);
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            Present present = data.OrderByDescending(p => p.Weight).FirstOrDefault();
+            return present;
+        }
+
+        public Present GetPresent(string name)
+        {
+            Present present = data.FirstOrDefault(x => x.Name == name);
+            return present;
+        }
+
+        public string Report()
+        {
+            return $"{this.Color} bag contains:{Environment.NewLine}" +
+                $"{string.Join(Environment.NewLine, data)}";
+            
+        }
+    }
+}
