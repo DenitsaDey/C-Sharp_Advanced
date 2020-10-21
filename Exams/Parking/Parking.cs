e3aada1b2ea769ba299c04ed3f8562f5f563a16e
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-21 14:01:37 +0300

    Add files via upload

---
+++ Exams/Parking/Parking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void Add(Car car)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Car car = data.Where(c => c.Manufacturer == manufacturer).FirstOrDefault(c => c.Model == model);
+            return data.Remove(car);
+        }
+
+        public Car GetLatestCar()
+        {
+            if (this.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Car car = data.OrderByDescending(c => c.Year).FirstOrDefault();
+                return car;
+            }
+        }
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car car = data.Where(c => c.Manufacturer == manufacturer).FirstOrDefault(c => c.Model == model);
+            if (data.Contains(car))
+            {
+                return car;
+            }
+            return null;
+        }
+
+        public string GetStatistics()
+        {
+            return $"The cars are parked in {this.Type}: {Environment.NewLine}{string.Join(Environment.NewLine, data)}";
+        }
+    }
+}
