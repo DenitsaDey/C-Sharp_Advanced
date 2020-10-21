227d86a769eda8cce312f04cc31a46bfb49f92bd
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-21 13:44:02 +0300

    Add files via upload

---
+++ Exams/Clinic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Pet pet)
+        {
+            if(Capacity > Count)
+            {
+                data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name);
+            return data.Remove(pet);
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = data.Where(p=>p.Owner == owner).FirstOrDefault(p => p.Name ==name);
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet pet = data.OrderByDescending(p => p.Age).FirstOrDefault();
+            if (this.data.Contains(pet))
+            {
+                return pet;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The clinic has the following patients:");
+            foreach (Pet pet in data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
