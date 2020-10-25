f45aa25763ae0c338abdd573a4632df48a277b29
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-25 20:31:30 +0300

    Add files via upload

---
+++ Exams/Classroom/Classroom.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if(this.Capacity > this.Count)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            if(students.Contains(student))
+            {
+                students.Remove(student);
+                return $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> wantedStudents = students.Where(s => s.Subject == subject).ToList();
+            if(wantedStudents.Count == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (Student student in wantedStudents)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+                return sb.ToString().TrimEnd();
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            return student;
+        }
+    }
+}
