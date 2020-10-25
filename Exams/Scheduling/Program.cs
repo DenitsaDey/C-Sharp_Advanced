6ad4d56760c88105657a0f5068cfa37c38cc0cae
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-25 20:26:03 +0300

    Add files via upload

---
+++ Exams/Scheduling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _25_Oct_Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            Queue<int> thread = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while(tasks.Peek() != taskToKill)
+            {
+                if(tasks.Peek() <= thread.Peek())
+                {
+                    tasks.Pop();
+                }
+                thread.Dequeue();
+            }
+            Console.WriteLine($"Thread with value {thread.Peek()} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", thread));
+        }
+    }
+}
