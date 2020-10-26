df0d7b9a058d6f8fd064578a5b52943ffcd3b4c7
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-26 10:29:36 +0300

    Add files via upload

---
+++ Stacks and Queues/04. Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> bracketIndex = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if(expression[i] == '(')
+                {
+                    bracketIndex.Push(i);
+                }
+                else if(expression[i] == ')')
+                {
+                    int startIndex = bracketIndex.Pop();
+                    string current = expression.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(current);
+                }
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/05. Print Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> myQ = new Queue<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    myQ.Enqueue(nums[i]);
+                }
+            }
+            Console.WriteLine(string.Join(", ", myQ));
+        }
+    }
+}
