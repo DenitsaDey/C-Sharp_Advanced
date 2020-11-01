97f876fb045b2f9d1f12d260d23bf3903397f9d5
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-01 05:30:32 +0300

    Add files via upload

---
+++ Functional Programming/02. Sum Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(array.Length);
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
---
+++ Functional Programming/03. Count Uppercase Word/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isUpperCase = x => x[0] == x.ToUpper()[0];
+            //Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Where(isUpperCase)
+            //    .ToArray()));
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUpperCase)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
