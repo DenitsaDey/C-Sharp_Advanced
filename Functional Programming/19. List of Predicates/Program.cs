33756aec012d0989de58672a9977313b28b29e50
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-01 05:41:59 +0300

    Add files via upload

---
+++ Functional Programming/19. List of Predicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _19._List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] division = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            List<int> result = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                bool flag = true;
+                for (int j = 0; j < division.Length; j++)
+                {
+                    Predicate<int> checker = x => i % x == 0;
+
+                    if (!checker(division[j]))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Functional Programming/22. TriFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _22._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Func<string, int, bool> checker = (x, y) =>
+           {
+               int sum = x.Select(c => (int)c).Sum();
+               
+               if(sum>= y)
+               {
+                   return true;
+               }
+               else
+               {
+                   return false;
+               }
+           };
+            foreach (var name in names)
+            {
+                if(checker(name, n))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            }
+        }
+    }
+}
