eff8531fa7412dedadedca0f06af459d77b79b68
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-01-05 11:22:25 +0300

    Add files via upload

---
+++ Defining Classes/DefiningClasses/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person person = this.People.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return person;
+        }
+    }
+}
---
+++ Defining Classes/DefiningClasses/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family myFamily = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                myFamily.AddMember(new Person(name, age));
+            }
+
+            //Console.WriteLine($"{myFamily.GetOldestMember().Name} {myFamily.GetOldestMember().Age}");
+            List<Person> sortedFamily = myFamily.People.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, sortedFamily));
+            //foreach (var person in sortedFamily)
+            //{
+            //    Console.WriteLine($"{person.Name} - {person.Age}");
+            //}
+        }
+    }
+}
