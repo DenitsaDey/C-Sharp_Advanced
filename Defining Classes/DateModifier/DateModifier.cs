f8d4c94b552011b63d6ac53aa4312faa6d37143a
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-01-05 11:20:47 +0300

    Add files via upload

---
+++ Defining Classes/DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static double GetDaysBetween(string dateOne, string dateTwo)
+        {
+            DateTime firstDate = DateTime.Parse(dateOne);
+            DateTime secondDate = DateTime.Parse(dateTwo);
+            double daysBetween = Math.Abs((firstDate - secondDate).TotalDays);
+            return daysBetween;
+        }
+    }
+}
---
+++ Defining Classes/DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+            double res = DateModifier.GetDaysBetween(dateOne, dateTwo);
+            Console.WriteLine(res);
+        }
+    }
+}
