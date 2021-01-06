80a1e767e9cb25e8eb95e455c0486f9365faeb81
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-01-06 22:45:04 +0300

    Add files via upload

---
+++ Basic Algorithms/Merge Sort/Mergesort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merge_Sort
+{
+    public class Mergesort<T> where T : IComparable
+    {
+        public static void Sort(T[] arr)
+        {
+
+        }
+        private static T[] aux;
+        private static void Merge(T[] arr, int lo, int mid, int hi)
+        {
+
+        }
+    }
+}
---
+++ Basic Algorithms/Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var result = Sum(arr);
+            Console.WriteLine(result);
+        }
+
+        private static int Sum(int[] arr, int index = 0)
+        {
+            if(index == arr.Length)
+            {
+                return 0;
+            }
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
